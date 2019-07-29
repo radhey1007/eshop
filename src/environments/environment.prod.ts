c9b9b0acfba9b20314ea52a30351d4bd6f91a433
radhey1007 <radheysv78@gmail.com>
2019-07-29 08:50:00 +0530

    first add component

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBkYDODDpXbTuodsvQ_GzH95Z9atY8vyO0",
+    authDomain: "e-shop-b7515.firebaseapp.com",
+    databaseURL: "https://e-shop-b7515.firebaseio.com",
+    projectId: "e-shop-b7515",
+    storageBucket: "",
+    messagingSenderId: "6921842844",
+    appId: "1:6921842844:web:8760d23702c4f173"     
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBkYDODDpXbTuodsvQ_GzH95Z9atY8vyO0",
+    authDomain: "e-shop-b7515.firebaseapp.com",
+    databaseURL: "https://e-shop-b7515.firebaseio.com",
+    projectId: "e-shop-b7515",
+    storageBucket: "",
+    messagingSenderId: "6921842844",
+    appId: "1:6921842844:web:8760d23702c4f173"     
+  }
 };
 
 /*

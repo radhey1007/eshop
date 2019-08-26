8bc6a46614877d1641109325609f00e92e5da337
Radhey Shyam <Radhey.Shyam@srmtechsol.com>
2019-08-26 10:35:23 +0530

    directive

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProductCardComponent {
 
-  @Input('product') product = [];
+  @Input('product') product:any = [];
   @Input('show-actions') showActions = true;
 
   
